fef7d5cb1f4c713a24ac2264d6bae80f5733ca5b
AbdelilahELMokhtari <abdelilah2021abdo@gmail.com>
2025-12-05 19:18:57 +0100

    Send message app for testing Toast

---
+++ app/src/main/java/com/example/tps/Exercice1.java
@@ -0,0 +1,37 @@
+package com.example.tps;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Exercice1 extends AppCompatActivity {
+    TextView t ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_exercice1);
+
+
+
+    }
+
+    public void displayMessage(View v){
+        t = (TextView) findViewById(R.id.message);
+        String message = t.getText().toString();
+        if(message.isEmpty()){
+            Toast.makeText(this, "Please enter your message", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
