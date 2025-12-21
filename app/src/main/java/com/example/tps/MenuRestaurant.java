2792e9bbba6a8c4a58f8a67c09490372cd3f36b3
AbdelilahELMokhtari <abdelilah2021abdo@gmail.com>
2025-12-21 15:41:32 +0100

    Menu Restaurant v2

--- app/src/main/java/com/example/tps/MenuRestaurant.java
+++ app/src/main/java/com/example/tps/MenuRestaurant.java
@@ -2,6 +2,7 @@ package com.example.tps;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -18,6 +19,7 @@ public class MenuRestaurant extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu_restaurant);
+
 
     }
 
@@ -36,10 +38,16 @@ public class MenuRestaurant extends AppCompatActivity {
         }else if (elementClicked.equals("tropicale")) {
             Toast.makeText(this, "Prix : 43DH", Toast.LENGTH_SHORT).show();
 
-        }else {
+        } else if (elementClicked.equals("btnabout")) {
+            Toast.makeText(this,"Hello , I am ABDELILAH EL MOKHTARI ,From IGOV",Toast.LENGTH_SHORT).show();
+        }else if (elementClicked.equals("btnexit")){
+            finish();
+        }
+        else {
             Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
 
         }
 
     }
+
 }
